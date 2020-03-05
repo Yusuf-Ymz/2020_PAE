8b58dc1db84a673eea85c2fbc0da53084dfb0aed
soumaya-izmar <49117683+soumaya-izmar@users.noreply.github.com>
2020-03-05 18:54:48 +0100

    refactor : changement menu deroulant

--- public/index.js
+++ public/index.js
@@ -7,25 +7,10 @@ $(document).ready(function () {
 
   token = initialisation();
  
+  //nav-bar 
   $('.leftmenu').on('click', function (e) {
     $('.slide-nav').toggleClass("active");
     e.preventDefault();
-  });
-
-  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-  $(".openbtn").on('click', function (e) {
-    document.getElementById("mySidebar").style.width = "240px";
-    //document.getElementById("main").style.marginLeft = "250px";
-    $(".carousel-control-prev").hide();
-    //$(".openbtn").hide();
-  });
-
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  $("#closebtn").on('click', function (e) {
-    $(".carousel-control-prev").show();
-    document.getElementById("mySidebar").style.width = "0";
-    //document.getElementById("main").style.marginLeft = "0";
-    //$(".openbtn").show();
   });
 
 
@@ -92,14 +77,15 @@ $(document).ready(function () {
 
 
 const  HideToHome = () =>{
-
   $("#nav_connect").show();
   $(".register").hide();
   $("#carouselExampleIndicators").show();
   $("#logo").show();
+  $("#logout").hide();
 }
 
 const HomeUser = () =>{
+  $("#logout").show();
   $("#login_message").html("");
   $("#nav_connect").hide();
   $(".register").hide();
