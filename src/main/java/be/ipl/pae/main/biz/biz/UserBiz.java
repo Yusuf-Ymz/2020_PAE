652322009675c711c6536d48066113b88ab5d8ea
Soumaya.Izmar <soumaya.izmar@student.vinci.be>
2020-03-05 12:24:26 +0100

    doc : ajout doc User.java

--- src/main/java/be/ipl/pae/main/biz/biz/UserBiz.java
+++ src/main/java/be/ipl/pae/main/biz/biz/UserBiz.java
@@ -3,5 +3,11 @@ package be.ipl.pae.main.biz.biz;
 import be.ipl.pae.main.biz.dto.UserDto;
 
 public interface UserBiz extends UserDto {
+  /**
+   * Si le mot de passe est corret ou pas
+   * 
+   * @param pwd : le mot de passe a verifié
+   * 
+   */
   boolean checkValidePassword(String pwd);
 }
