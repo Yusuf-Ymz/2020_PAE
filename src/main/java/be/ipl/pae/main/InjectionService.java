22902c34ad6959e607bff6ddd512ddeea1be609d
Mustapha <Mustapha@LAPTOP-V0DTSAL5>
2020-03-05 21:42:06 +0100

    doc : ajout de la documentation pour la class InjectionService

--- src/main/java/be/ipl/pae/main/InjectionService.java
+++ src/main/java/be/ipl/pae/main/InjectionService.java
@@ -21,6 +21,14 @@ public class InjectionService {
     }
   }
 
+
+  /**
+   * 
+   * renvoie la valeur de la propiete passée en paramétre.
+   * 
+   * @param propriete le nom (la clé) de la propriete recherché
+   * @return la valeur de la propriete.
+   */
   public static String getConfiguration(String propriete) {
     if (dependencies.containsKey(propriete)) {
       return (String) dependencies.get(propriete);
@@ -30,6 +38,14 @@ public class InjectionService {
     return value;
   }
 
+  /**
+   * 
+   * Crée par introspection l'implementation de l'interface passée en parametre et la renvoie.
+   * 
+   * @param c la class de l'interface dont on veut l'implementation
+   * @param params la liste des paramétres à fournir pour pouvoir instancier l'objet
+   * @return l'objet créer
+   */
   public static <T> T getDependency(Class<?> c, Object... params) {
     String implName = props.getProperty(c.getName());
     if (dependencies.containsKey(implName)) {
@@ -39,17 +55,18 @@ public class InjectionService {
       Class<?> implClass = Class.forName(implName);
       for (Constructor<?> constructeur : implClass.getDeclaredConstructors()) {
         Class<?>[] paramsConstruct = constructeur.getParameterTypes();
-        if (params.length != paramsConstruct.length) {
-          continue;
+        if (params.length == paramsConstruct.length) {
+          constructeur.setAccessible(true);
+          Object obj = constructeur.newInstance(params);
+          dependencies.put(implName, obj);
+          return (T) obj;
         }
-        constructeur.setAccessible(true);
-        return (T) constructeur.newInstance(params);
       }
-    } catch (Throwable e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+      return null;
+    } catch (Throwable exception) {
+      exception.printStackTrace();
+      throw new RuntimeException(exception);
     }
-    return null;
   }
 
 }
