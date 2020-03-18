1a6c16cac0a0cae6e54f30911ab50ec52d7cc057
Antoine Visschers <antoine.visschers@student.vinci.be>
2020-03-18 22:05:07 +0100

    fix: résolution du problème de gitlab

--- src/main/java/be/ipl/pae/ihm/UserServlet.java
+++ src/main/java/be/ipl/pae/ihm/UserServlet.java
@@ -60,16 +60,7 @@ public class UserServlet extends HttpServlet {
       JWTVerifier verifier = JWT.require(algorithm).build();
       DecodedJWT jwt = verifier.verify(token);
       int userId = jwt.getClaim("id").asInt();
-<<<<<<< HEAD
-=======
-      UserDto userConnecte = userUcc.obtenirUser(userId);
-      if (userConnecte.isOuvrier()) {
-        if (req.getParameter("action").equals("listeUser")) {
-          List<UserDto> listeUser = userUcc.listerUsers();
-          json = "{\"listeUser\":" + genson.serialize(listeUser) + "}";
-          status = HttpServletResponse.SC_OK;
-        }
->>>>>>> refs/remotes/origin/master
+
 
       Genson genson = new Genson();
       Map<String, Object> map = genson.deserialize(req.getReader(), Map.class);
