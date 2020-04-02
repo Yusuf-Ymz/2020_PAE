fba0fa61c8c6911ad52e662e47493d41aea6cf5e
Mustapha <Mustapha@LAPTOP-V0DTSAL5>
2020-04-02 16:37:16 +0200

    fix : correction des checkstyles

--- src/main/java/be/ipl/pae/bizz/ucc/AmenagementUcc.java
+++ src/main/java/be/ipl/pae/bizz/ucc/AmenagementUcc.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface AmenagementUcc {
 
   /**
-   * Liste tous les aménagements
+   * Liste tous les aménagements.
    * 
    * @return la liste des aménagements
    */
